da4d7b1a66b796177840b07373ee3e369926854c
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-17 10:31:37 -0500

    fix: Se actualizó pin front-shared a 7921b9c en js/boot/loader.ts

--- js/boot/loader.ts
+++ js/boot/loader.ts
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const FRONT_SHARED_REF = "9f0ec53";
+  const FRONT_SHARED_REF = "7921b9c";
   const BOOT_HELPER =
     "https://cdn.jsdelivr.net/gh/Jeff-Aporta/front-shared@" + FRONT_SHARED_REF + "/cdn/boot-helper.mjs?v=" + FRONT_SHARED_REF;
 
